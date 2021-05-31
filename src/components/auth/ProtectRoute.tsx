788db8d12e34556f4c8240c638db791c2eb072b5
t-naoi <t-naoi@i-enter.co.jp>
2021-05-31 14:47:13 +0900

    firebase.User add

--- src/components/auth/ProtectRoute.tsx
+++ src/components/auth/ProtectRoute.tsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from 'react'
+import firebase from 'firebase'
 import { useRouter } from 'next/router'
 import { auth } from '~/utils/firebase'
 
 export const ProtectRoute = ({ children }) => {
   const router = useRouter()
-  const [currentUser, setCurrentUser] = useState<null | object>(null)
+  const [currentUser, setCurrentUser] = useState<null | firebase.User>(null)
 
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
